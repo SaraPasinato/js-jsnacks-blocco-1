0645708a7027970306a71907497ec552be853228
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-07 15:48:16 +0200

    add snack3

---
+++ js/snack3.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * SNACK 3: Generatore di “nomi cognomi” casuali: prendendo 
+ * una lista di nomi e una lista di cognomi, Gatsby vuole 
+ * generare una falsa lista di 3 invitati.
+ * 
+ */
+// ** 1: inizializzo nomi e cognomi negli array dedicati
+var arrName = ["Mammolo", "Pisolo", "Eolo", "Dotto", "Brontolo", "Cucciolo", "Gongolo"];
+var arrSurname = ["Bianchi", "Rossi", "Verdi", "Neri"];
+var invitedName = [];
+
+var randName;
+var randSurname;
+var msg = "";
+var result = document.getElementById('result');
+
+// ** 2: controllo che siano meno di 3 gli invitati
+
+while (invitedName.length < 3) {
+    // ** 3: numeri casuali per l'indice del vettore
+
+    randName = Math.floor(Math.random() * arrName.length);
+    randSurname = Math.floor(Math.random() * arrSurname.length);
+    // ** 4: compondo il nome per poterlo memorizzare in invitedName
+    var fullName = arrName[randName] + " " + arrSurname[randSurname];
+    // ** 5: testo per output
+    msg += fullName + "\n";
+    // ** 6: inserico fullName in coda al vettore 
+    invitedName.push(fullName);
+}
+
+// ** 7: inset output in tag html
+
+result.innerText = msg;
+
+console.table(invitedName);
